9eb23a3abf40d2bda5fe6bf7826130d610bf046a
keyaki1125 <horiguchitokio@gmail.com>
2022-02-14 00:34:14 +0900

    update

--- bucket_sort.py
+++ bucket_sort.py
@@ -1,16 +1,26 @@
 from typing import List
 
+"""
+Bucket sort:
+insertion sortを使用。
+・任意の要素数のリスト（バケット）を用意
+・今回はリスト内の最大値を10で割った、10個のリストにする
+・バケット[0]には10で割って商が0のものを、バケット[1]には10で割って商が1のものを振り分けていく
+・二次元配列のバケットの各要素内でinsertion sortをかける。
+・バケット内の値を連結する
+"""
+
 
 def insertion_sort(numbers: List[int]) -> List[int]:
     len_numbers = len(numbers)
     for i in range(1, len_numbers):
-        temp = numbers[i]
+        temp = numbers[i]  # 入れ替えがあればこの値が前に進んでいく
         j = i - 1
         while j >= 0 and numbers[j] > temp:
-            numbers[j + 1] = numbers[j]
-            j -= 1
+            numbers[j+1] = numbers[j]  # numbers[j+1]はtempと同じ(tempは更新したくない)
+            j -= 1  # -1でnumbers[j] > tempになるまでバックしていく
 
-        numbers[j + 1] = temp
+        numbers[j+1] = temp
 
     return numbers
 
@@ -18,25 +28,23 @@ def insertion_sort(numbers: List[int]) -> List[int]:
 def bucket_sort(numbers: List[int]) -> List[int]:
     max_num = max(numbers)
     len_numbers = len(numbers)
-    size = max_num // len_numbers
+    size = max_num // len_numbers  # bucketの要素数となる(//は割り算の整数部分を返す)
+    buckets = [[] for _ in range(size)]  # リスト内包表記
 
-    buckets = [[] for _ in range(size)]
     for num in numbers:
-        i = num // size
-        if i != size:
+        i = num // size  # 要素を10で割る
+        if i != size:  # 今回の場合、10台は1に、20台は2というふうに振り分けられる、
             buckets[i].append(num)
-        else:
+        else:  # size以上のもの（今回は100）は一つ手前に振り分ける
             buckets[size - 1].append(num)
 
-    print(buckets)
-
     for i in range(size):
+        # bucket各要素内でinsertion sortをかける
         insertion_sort(buckets[i])
-
-    print(buckets)
 
     result = []
     for i in range(size):
+        # resultにバケット内の値を連結していく
         result += buckets[i]
 
     return result
@@ -46,5 +54,5 @@ if __name__ == '__main__':
     import random
 
     nums = [random.randint(0, 1000) for _ in range(10)]
-    nums = [1, 5, 28, 25, 100, 52, 27, 91, 22, 99]
+    # nums = [1, 5, 28, 25, 100, 52, 27, 91, 22, 99]
     print(bucket_sort(nums))
--- shell_sort.py
+++ shell_sort.py
@@ -1,5 +1,12 @@
 from typing import List
 
+"""
+shell sort:
+insertion sortの改良
+gap = n / 2
+
+"""
+
 
 def shell_sort(numbers: List[int]) -> List[int]:
     len_numbers = len(numbers)
@@ -7,14 +14,30 @@ def shell_sort(numbers: List[int]) -> List[int]:
     while gap > 0:
         for i in range(gap, len_numbers):
             temp = numbers[i]
-            j = i
+            j = i # 3
             while j >= gap and numbers[j - gap] > temp:
-                numbers[j] = numbers[j - gap]
+                numbers[j] = numbers[j-gap]
                 j -= gap
             numbers[j] = temp
         gap //= 2
 
     return numbers
+
+
+# def shell_sort(numbers: List[int]) -> List[int]:
+#     len_numbers = len(numbers)
+#     gap = len_numbers // 2
+#     while gap > 0:
+#         for i in range(gap, len_numbers):
+#             temp = numbers[i]
+#             j = i
+#             while j >= gap and numbers[j - gap] > temp:
+#                 numbers[j] = numbers[j - gap]
+#                 j -= gap
+#             numbers[j] = temp
+#         gap //= 2
+#
+#     return numbers
 
 
 if __name__ == '__main__':
